1ff40e05fb7a905b29f97e546cffae2d53637100
Łukasz Gmyrek <lukasz.gmyrek@gmail.com>
2015-03-29 22:42:08 +0200

    initial

---
+++ desktop/src/pl/edu/agh/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.edu.agh.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
