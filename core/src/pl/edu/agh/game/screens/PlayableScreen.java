b6de1b6ee533eaf8b7ef564ca603bd6447a07cb1
lgmyrek <lukasz.gmyrek@gmail.com>
2015-04-16 22:16:43 +0200

    Changing main game class from ScreenAdapter to Game to allow more complex screen changing.

---
+++ core/src/pl/edu/agh/game/screens/PlayableScreen.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.game.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by lgmyrek on 4/16/15.
+ */
+public class PlayableScreen extends ScreenAdapter {
+}
--- desktop/src/pl/edu/agh/game/desktop/DesktopLauncher.java
+++ desktop/src/pl/edu/agh/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package pl.edu.agh.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import pl.edu.agh.game.Game;
+import pl.edu.agh.game.CleaverOfDoom;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new CleaverOfDoom(), config);
 	}
 }
