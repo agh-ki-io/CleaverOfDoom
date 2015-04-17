f101235ebc9cfab72bf3397989dde80a2a4e7944
mkasprz <mkasprz@student.agh.edu.pl>
2015-04-17 11:09:12 +0200

    MainMenu - WIP

--- core/src/pl/edu/agh/game/screens/MenuScreen.java
+++ core/src/pl/edu/agh/game/screens/MenuScreen.java
@@ -4,21 +4,40 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import pl.edu.agh.game.CleaverOfDoom;
 
-/**
- * Created by lgmyrek on 4/16/15.
- */
 public class MenuScreen implements Screen {
     private final CleaverOfDoom game;
     SpriteBatch batch;
     Texture img;
+    private Stage stage;
 
     public MenuScreen(CleaverOfDoom game) {
         this.game = game;
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+//        img = new Texture("badlogic.jpg");
+//        Gdx.input.setInputProcessor(stage);
+        TextButton startButton = new TextButton("Start", new TextButton.TextButtonStyle(null, null, null, new BitmapFont()));
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("clicked");
+                resume();
+            }
+        });
+
+        stage.addActor(startButton);
+
+
     }
 
     @Override
@@ -30,8 +49,12 @@ public class MenuScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
-        batch.draw(img, 0, 0);
+
+        stage.draw();
+
+        //batch.draw(img, 0, 0);
         batch.end();
     }
 
@@ -47,7 +70,7 @@ public class MenuScreen implements Screen {
 
     @Override
     public void resume() {
-
+        game.setScreen(new PlayableScreen());
     }
 
     @Override
